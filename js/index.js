384da18f6495b2f32cbe2bb07484919c88530056
orsoli <fljcoli@gmail.com>
2024-09-04 16:37:55 +0200

    added html structure part

--- js/index.js
+++ js/index.js
@@ -5,33 +5,39 @@ const app = createApp({
   data() {
     return {
       // Create variable list of img and data img
-      mediaList: [
+      media: [
         {
+          id: 1,
           image: "img/01.webp",
           title: `Marvel's Spiderman Miles Morale`,
           text: "Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.",
         },
         {
+          id: 2,
           image: "img/02.webp",
           title: "Ratchet & Clank: Rift Apart",
           text: "Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.",
         },
         {
+          id: 3,
           image: "img/03.webp",
           title: "Fortnite",
           text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
         },
         {
+          id: 4,
           image: "img/04.webp",
           title: "Stray",
           text: "Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city",
         },
         {
+          id: 5,
           image: "img/05.webp",
           title: "Marvel's Avengers",
           text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
         },
       ],
+      activeIndex: 0, // activeIndex initial value
     };
   },
 }).mount("#app");
---
+++ js/structure.js
@@ -0,0 +1,15 @@
+/*
+   Starting from the markup of the version done in plain JavaScript, 
+   remake the slider, but this time using Vue.
+*/
+
+//--- Preparation phase
+//? Preapre HTML structure
+//JS:
+// Destructuring createApp from vue object
+// Create an instace of vue app
+// Create an variable and active index with initial value
+// Define functions increment and decremetn active index
+
+//---Output phase
+// Mount in DOM element
